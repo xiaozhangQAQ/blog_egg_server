8f889a25c2bff219ee695e7f6097422193ccb150
zhangjm <zhangjm@icarplus.net>
2020-05-29 17:55:25 +0800

    userInfo

---
+++ app/service/home.js
@@ -0,0 +1,21 @@
+const Service = require('egg').Service;
+
+class HomeService extends Service {
+    async total(params) {
+      const { mongoose } = this.app;
+      let result = {};
+      let datas = await this.ctx.model.Article.find({uid:params._id}).count();
+      result={
+        code:20000,
+        data:{
+            total:datas
+        },
+        errmsg:'success'
+      };
+      return result;     
+    };
+
+
+  }
+
+  module.exports = HomeService;
